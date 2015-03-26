a94dfa8852fff57b541a5548c601391892e5a0c4
timothy <t.williams.im@gmail.com>
2015-03-26 02:54:22 -0400

    the reaction time game is functional

--- reactiontester-script.js
+++ reactiontester-script.js
@@ -1,13 +1,30 @@
-document.getElementById("red-box").onclick=function(){
-  //document.getElementById("red-box").style.display="none";
-  this.style.display="none";
 
+var clickedTime; var createdTime; var reactionTime;
+
+function makeBox(){
+  createdTime = Date.now();
   //var time = Math.random();value between 0 and 1
   //time = time*5000; this works too
-
-  var random_time = Math.floor((Math.random()* 5000) + 1);
+  var random_time = Math.floor((Math.random()* 5000) + 1);//gives whole int
   setTimeout(function(){
     document.getElementById("red-box").style.display="block";
     //alert("hello");
   }, random_time);
 }
+
+var time = Date.now();
+console.log(time);
+
+
+
+document.getElementById("red-box").onclick=function(){
+  clickedTime = Date.now();
+  //document.getElementById("red-box").style.display="none";
+  reactionTime = (clickedTime - createdTime)/1000;
+  console.log(reactionTime);
+  document.getElementById("time").innerHTML=reactionTime + " seconds";
+  this.style.display="none";
+  makeBox();
+}
+
+makeBox();
