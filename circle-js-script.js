74345fc5d9c787bd706406161dca38d6029d1253
timothy <t.williams.im@gmail.com>
2015-03-26 00:34:09 -0400

    put circle.html script in a separate file

---
+++ circle-js-script.js
@@ -0,0 +1,5 @@
+document.getElementById("red-circle").onclick=function(){
+  /*var elem = document.getElementById("red-circle");
+  elem.parentNode.removeChild(elem);*///works as well
+  document.getElementById("red-circle").style.display="none";
+}
