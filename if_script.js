e7cc6cd1ec93df6890fbdc0334c8f863e380e5c0
timothy <t.williams.im@gmail.com>
2015-03-26 00:24:59 -0400

    separated the if script into a separate file

---
+++ if_script.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick=function() {
+  if (document.getElementById("weather").value=="rain") {
+    alert ("it's raining");
+  } else {
+    alert("it's not raining!");
+  }
+}
